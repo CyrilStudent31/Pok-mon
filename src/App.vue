efa147bd8131a4658d03eb76248699a21ea8426a
Prozzinho <pradatchristopher@gmail.com>
2025-12-15 16:02:48 +0100

    Ajout de tout pleins de trucs

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,10 @@
 <template>
-  <!-- <div class="min-h-screen flex flex-col bg-base-200 text-base-content">
-    <AppHeader /> -->
-    <!-- Conteneur principal du contenu, centré et responsive -->
     <main class="flex-1 w-full max-w-7xl mx-auto p-4 md:p-8">
       <!-- Ici on aura la router view -->
       <router-view></router-view>
-      <!-- <RouterView></RouterView> -->
+
     </main>
-    <!-- Footer en bas de la page -->
-    <!-- <AppFooter />
-  </div> -->
 </template>
 
 <script setup>
-// import AppHeader from './components/AppHeader.vue';
-// import AppFooter from './components/AppFooter.vue';
-// import HelloWorld from './components/HelloWorld.vue';
 </script>
-
-
-
-
-
-
-
-
-<!-- <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>Pokémon</h1>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    header {
+        background-color: yellow;
+        padding: 10px;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
