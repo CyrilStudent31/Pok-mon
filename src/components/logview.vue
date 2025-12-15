49aef2fd0026825653130f1d523e8d828ae2a2c3
jose <balnelius19@gmail.com>
2025-12-15 15:58:22 +0100

    log 1erepartie

---
+++ src/components/logview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-4">
+    <h5>Battle Log</h5>
+    <ul class="list-group">
+      <li v-for="(entry, index) in log" :key="index" class="list-group-item">
+        {{ entry }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useCombatStore } from '@/stores/combatStore'
+
+// On récupère le log depuis le store
+const { log } = storeToRefs(useCombatStore())
+</script>
